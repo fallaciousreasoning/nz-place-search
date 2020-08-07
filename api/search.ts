41ab5b705aa7cac339225832dc3716feaa5ee580
Jay Harris <jay.harris@outlook.co.nz>
2020-08-07 19:55:53 +1200

    Set content type

--- api/search.ts
+++ api/search.ts
@@ -1,5 +1,6 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
 export default (req: NowRequest, res: NowResponse) => {
+  res.setHeader("Content-Type", "application/json");
   res.json({ name: 'JohnFoo', email: 'john@example.com' });
 }
