29a00a7d61073d016f112a2acaf4a8b9ab0a8efc
Jay Harris <jay.harris@outlook.co.nz>
2020-08-07 19:56:38 +1200

    Add places

--- api/search.ts
+++ api/search.ts
@@ -1,5 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
+const places = require("../data/min_nz_places.json");
+
 export default (req: NowRequest, res: NowResponse) => {
   res.setHeader("Content-Type", "application/json");
   res.json({ name: 'JohnFoo', email: 'john@example.com' });
