196e4359d3cff5114bcc6ae527434a1858a67afd
Jay Harris <jay.harris@outlook.co.nz>
2020-08-07 19:57:33 +1200

    Add filter function

--- api/search.ts
+++ api/search.ts
@@ -1,6 +1,17 @@
 import { NowRequest, NowResponse } from '@vercel/node'
+import { SearchPlace } from '../searchPlace';
 
-const places = require("../data/min_nz_places.json");
+const places: SearchPlace[] = require("../data/min_nz_places.json");
+
+function* filterPlaces(query: string) {
+  for (const place of places) {
+      const name = place.name;
+      if (!name) continue;
+      if (!name.includes(query)) continue;
+
+      yield place;
+  }
+}
 
 export default (req: NowRequest, res: NowResponse) => {
   res.setHeader("Content-Type", "application/json");
