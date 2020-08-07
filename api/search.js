a339a8e3c2288de1d401588a319435b65d6f5808
Jay Harris <jay.harris@outlook.co.nz>
2020-08-07 19:02:52 +1200

    Hacky api hack is hacky?

---
+++ api/search.js
@@ -0,0 +1,19 @@
+const data = require("../data/min_nz_places.json");
+
+function* filterPlaces(query) {
+    for (const place of places) {
+        const name = place.name;
+        if (!name) continue;
+        if (!name.includes(query)) continue;
+
+        yield place;
+    }
+}
+
+export default (req, res) => {
+    const query = req.params.query;
+    const matchingPlaces = Array.from(filterPlaces(query));
+
+    res.setHeader("Content-Type", "application/json");
+    res.send(matchingPlaces);
+}
