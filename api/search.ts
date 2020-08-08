ab3522067b12ec2ef1fb9889cadf9dca9bc52911
Jay Harris <jay.harris@outlook.co.nz>
2020-08-08 12:01:05 +1200

    Go back

--- api/search.ts
+++ api/search.ts
@@ -4,10 +4,21 @@ import { SearchPlace } from '../searchPlace';
 const places: SearchPlace[] = require("../data/min_nz_places.json");
 
 function* filterPlaces(query: string) {
+  const queries = [query.toLowerCase()];
+  // Search for "Mount Foo" as well as "Mt Foo"
+  if (queries[0].includes("mt"))
+    queries.push(queries[0].replace("mt", "mount"));
+  // Search for "Mt Foo" as well as "Mount Foo"
+  if (queries[0].includes("mount"))
+    queries.push(queries[0].replace("mount", "mt"));
+
   for (const place of places) {
       const name = place.name;
       if (!name) continue;
-      if (!name.toLowerCase().includes(query.toLowerCase())) continue;
+
+      const lowerName = name.toLowerCase();
+      if (!queries.some(q => lowerName.includes(q)))
+        continue;
 
       yield place;
   }
